b40506f88f9653be9d18ec8d506e9a30fd817bea
Lingraj Mahanand <lmahanand@gmail.com>
2016-10-23 23:35:02 +0530

    JWT authentication implementation

--- src/main/java/com/mediaocean/onlinestore/resource/MainResource.java
+++ src/main/java/com/mediaocean/onlinestore/resource/MainResource.java
@@ -1,15 +1,33 @@
 package com.mediaocean.onlinestore.resource;
 
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
+import javax.json.stream.JsonGenerationException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
+import org.apache.log4j.Logger;
+import org.jose4j.jwk.JsonWebKey;
+import org.jose4j.jwk.JsonWebKeySet;
+import org.jose4j.jwk.RsaJsonWebKey;
+import org.jose4j.jwk.RsaJwkGenerator;
+import org.jose4j.jws.AlgorithmIdentifiers;
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.consumer.InvalidJwtException;
+import org.jose4j.jwt.consumer.JwtConsumer;
+import org.jose4j.jwt.consumer.JwtConsumerBuilder;
+import org.jose4j.lang.JoseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -17,12 +35,35 @@ import org.springframework.stereotype.Component;
 import com.mediaocean.onlinestore.api.facade.BillingService;
 import com.mediaocean.onlinestore.domain.model.ItemizedBill;
 import com.mediaocean.onlinestore.domain.model.Product;
+import com.mediaocean.onlinestore.model.StatusMessage;
+import com.mediaocean.onlinestore.model.User;
+import com.mediaocean.onlinestore.util.UserValidation;
 
 @Component
 @Path("resource")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class MainResource {
+
+	static Logger logger = Logger.getLogger(MainResource.class);
+	static List<JsonWebKey> jwkList = null;
+
+	static {
+		logger.info("Inside static initializer...");
+		jwkList = new LinkedList<>();
+		for (int kid = 1; kid <= 3; kid++) {
+			JsonWebKey jwk = null;
+			try {
+				jwk = RsaJwkGenerator.generateJwk(2048);
+				logger.info("PUBLIC KEY (" + kid + "): " + jwk.toJson(JsonWebKey.OutputControlLevel.PUBLIC_ONLY));
+			} catch (JoseException e) {
+				e.printStackTrace();
+			}
+			jwk.setKeyId(String.valueOf(kid));
+			jwkList.add(jwk);
+		}
+	}
+
 	@Autowired
 	@Qualifier(value = "billingService")
 	private BillingService billingService;
@@ -33,12 +74,108 @@ public class MainResource {
 		return "Checkout counter is up and running!";
 	}
 
+	@Path("authenticate")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response authenticateCredentials(@HeaderParam("username") String username,
+			@HeaderParam("password") String password) throws JsonGenerationException, IOException {
+		logger.info("Authenticating User Credentials...");
+
+		if (username == null) {
+			StatusMessage statusMessage = new StatusMessage();
+			statusMessage.setStatus(Status.PRECONDITION_FAILED.getStatusCode());
+			statusMessage.setMessage("Username value is missing.");
+			return Response.status(Status.PRECONDITION_FAILED.getStatusCode()).entity(statusMessage).build();
+		}
+
+		if (password == null) {
+			StatusMessage statusMessage = new StatusMessage();
+			statusMessage.setStatus(Status.PRECONDITION_FAILED.getStatusCode());
+			statusMessage.setMessage("Password value is missing.");
+			return Response.status(Status.PRECONDITION_FAILED.getStatusCode()).entity(statusMessage).build();
+		}
+
+		User user = UserValidation.validateUser(username, password);
+
+		if (user == null) {
+			StatusMessage statusMessage = new StatusMessage();
+			statusMessage.setStatus(Status.FORBIDDEN.getStatusCode());
+			statusMessage.setMessage("Access Denied for this functionality.");
+			return Response.status(Status.FORBIDDEN.getStatusCode()).entity(statusMessage).build();
+		}
+
+		RsaJsonWebKey senderJwk = (RsaJsonWebKey) jwkList.get(0);
+
+		senderJwk.setKeyId("1");
+		logger.info("JWK (1) ===> " + senderJwk.toJson());
+
+		// Create the Claims, which will be the content of the JWT
+		JwtClaims claims = new JwtClaims();
+		claims.setIssuer("billCheckout.com");
+		claims.setExpirationTimeMinutesInTheFuture(10);
+		claims.setGeneratedJwtId();
+		claims.setIssuedAtToNow();
+		claims.setNotBeforeMinutesInThePast(2);
+		claims.setSubject(user.getUsername());
+		claims.setStringListClaim("roles", user.getRolesList());
+
+		JsonWebSignature jws = new JsonWebSignature();
+
+		jws.setPayload(claims.toJson());
+
+		jws.setKeyIdHeaderValue(senderJwk.getKeyId());
+		jws.setKey(senderJwk.getPrivateKey());
+
+		jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.RSA_USING_SHA256);
+
+		String jwt = null;
+		try {
+			jwt = jws.getCompactSerialization();
+		} catch (JoseException e) {
+			e.printStackTrace();
+		}
+
+		return Response.status(200).entity(jwt).build();
+
+	}
+
 	@POST
 	@Path("itemizedBill")
-	public Response calculateItemizedBill(final Product[] products) {
+	public Response calculateItemizedBill(@HeaderParam("token") String token, final Product[] products)
+			throws JsonGenerationException, IOException {
+
+		logger.info("Inside calculateItemizedBill...");
+
+		if (token == null) {
+			StatusMessage statusMessage = new StatusMessage();
+			statusMessage.setStatus(Status.FORBIDDEN.getStatusCode());
+			statusMessage.setMessage("Access Denied for this functionality.");
+			return Response.status(Status.FORBIDDEN.getStatusCode()).entity(statusMessage).build();
+		}
+
+		JsonWebKeySet jwks = new JsonWebKeySet(jwkList);
+		JsonWebKey jwk = jwks.findJsonWebKey("1", null, null, null);
+		logger.info("JWK (1) ===> " + jwk.toJson());
+
+		// Validate Token's authenticity and check claims
+		JwtConsumer jwtConsumer = new JwtConsumerBuilder().setRequireExpirationTime().setAllowedClockSkewInSeconds(30)
+				.setRequireSubject().setExpectedIssuer("billcheckout.com").setVerificationKey(jwk.getKey()).build();
+
+		try {
+			// Validate the JWT and process it to the Claims
+			JwtClaims jwtClaims = jwtConsumer.processToClaims(token);
+			logger.info("JWT validation succeeded! " + jwtClaims);
+		} catch (InvalidJwtException e) {
+			logger.error("JWT is Invalid: " + e);
+			StatusMessage statusMessage = new StatusMessage();
+			statusMessage.setStatus(Status.FORBIDDEN.getStatusCode());
+			statusMessage.setMessage("Access Denied for this functionality.");
+			return Response.status(Status.FORBIDDEN.getStatusCode()).entity(statusMessage).build();
+		}
 
 		ItemizedBill itemizedBill = new ItemizedBill();
 		itemizedBill = billingService.generateItemizedBill(Arrays.asList(products));
 		return Response.status(201).entity(itemizedBill).build();
 	}
+
 }
